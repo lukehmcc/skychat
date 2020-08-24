68a5acef786c387c25c5a8f31d0f327cb95e1b17
George Carlson <gcarlson@adc4gis.com>
2020-08-24 18:50:41 -0500

    Import JS Should Use Relative Path

--- js/modal/new_chat.js
+++ js/modal/new_chat.js
@@ -1,22 +1,36 @@
 function newChat() {
     showModal("new_chat_modal");
-    const pin = window.location.pathname.substring(1, 47);
-    fetch(`/${pin}/`)
-        .then(response => response.text())
-        .then(result => {
-            const host = `${getRandomString(26)}`;
+    const host = `${getRandomString(26)}`;
+    var formData = new FormData();    
+    $.when(
+        $.get(`index.html`, function( data ) {
             const skychatDiv = `<skychat id="skychat_host" style="display: none">`;
-            const endingIndex = result.lastIndexOf(skychatDiv);
-            const newPage = result.substring(0, endingIndex) 
+            const endingIndex = data.lastIndexOf(skychatDiv);
+            const newPage = data.substring(0, endingIndex) 
             + `${skychatDiv}${host}</skychat><skychat id="skychat_epoch" style="display: none">${getEpochDay(1)}</skychat></body></html>`;
             const blob = new Blob([newPage],{ type: 'text/html' });
-            var formData = new FormData();
-            formData.append('file', blob);
-            fetch(`${getUploadPath(getNetwork())}${host}?filename=skychat.html`, {method: 'POST',body: formData})
-                .then(response => response.json())
-                .then(result => {
-                    window.location.pathname = `/${result[getUploadResponseKey(getNetwork())]}/`;
-                })
-                .catch(error => {console.error('Error:', error)});
+            formData.append('file', blob, 'index.html')}),
+        $.get(`js/archive_comments.js`, function( data ) {formData.append('file', new Blob([data],{ type: 'text/html' }), 'js/archive_comments.js')}),
+        $.get(`js/comments.js`, function( data ) {formData.append('file', new Blob([data],{ type: 'application/json' }), 'js/comments.js')}),
+        $.get(`js/crypto.js`, function( data ) {formData.append('file', new Blob([data],{ type: 'application/json' }), 'js/crypto.js')}),
+        $.get(`js/main.js`, function( data ) {formData.append('file', new Blob([data],{ type: 'application/json' }), 'js/main.js')}),
+        $.get(`js/modal/compose.js`, function( data ) {formData.append('file', new Blob([data],{ type: 'application/json' }), 'js/modal/compose.js')}),
+        $.get(`js/modal/login.js`, function( data ) {formData.append('file', new Blob([data],{ type: 'application/json' }), 'js/modal/login.js')}),
+        $.get(`js/modal/modal.js`, function( data ) {formData.append('file', new Blob([data],{ type: 'application/json' }), 'js/modal/modal.js')}),
+        $.get(`js/modal/new_chat.js`, function( data ) {formData.append('file', new Blob([data],{ type: 'application/json' }), 'js/modal/new_chat.js')}),
+        $.get(`js/modal/permalink.js`, function( data ) {formData.append('file', new Blob([data],{ type: 'application/json' }), 'js/modal/permalink.js')}),
+        $.get(`js/modal/portal.js`, function( data ) {formData.append('file', new Blob([data],{ type: 'application/json' }), 'js/modal/portal.js')}),
+        $.get(`js/modal/upload.js`, function( data ) {formData.append('file', new Blob([data],{ type: 'application/json' }), 'js/modal/upload.js')}),
+        $.get(`js/initiate.js`, function( data ) {formData.append('file', new Blob([data],{ type: 'application/json' }), 'js/initiate.js')}),
+        $.Deferred(function( deferred ){
+            $( deferred.resolve );
         })
+    ).done(function(){
+        fetch(`${getUploadPath(getNetwork())}${host}?filename=${host}`, {method: 'POST',body: formData})
+            .then(response => response.json())
+            .then(result => {
+                window.location.pathname = `/${result[getUploadResponseKey(getNetwork())]}/`;
+            })
+            .catch(error => {console.error('Error:', error)});
+    });
 }
--- js/modal/portal.js
+++ js/modal/portal.js
@@ -10,24 +10,41 @@ function portal() {
             window.location.href = `${thePortal}/${window.location.pathname.substring(1, 47)}/?secret=${getSecret()}`;
         })
         .catch(error => {
-            const pin = window.location.pathname.substring(1, 47);
-            fetch(`/${pin}/`)
-                .then(response => response.text())
-                .then(result => {
-                    var theNetwork = getNetwork() == "sia" ? "prime" : "sia";
-                    var formData = new FormData();
-                    formData.append('file', new Blob([result],{ type: 'text/html' }));
-                    fetch(`${thePortal}${getUploadPath(theNetwork)}${getHost()}?filename=skychat.html&force=true`, {method: 'POST',body: formData})
-                        .then(response => response.json())
-                        .then(result => {
-                            window.location.href = `${thePortal}/${result[getUploadResponseKey(theNetwork)]}/?secret=${getSecret()}`;
-                        })
-                        .catch(error => {
-                            document.getElementById('portal-trigger-container').style.display = `block`;
-                            document.getElementById('portal-loading-container').style.display = `none`;
-                            alert(`Unable to chang portal to ${thePortal}`);
-                            console.error('Error:', error)
-                        });
-                })
+
+var theNetwork = getNetwork() == "sia" ? "prime" : "sia";
+    var formData = new FormData();
+    $.when(
+        $.get(`index.html`, function( data ) {formData.append('file', new Blob([data],{ type: 'text/html' }), 'index.html')}),
+        $.get(`js/archive_comments.js`, function( data ) {formData.append('file', new Blob([data],{ type: 'application/json' }), 'js/archive_comments.js')}),
+        $.get(`js/comments.js`, function( data ) {formData.append('file', new Blob([data],{ type: 'application/json' }), 'js/comments.js')}),
+        $.get(`js/crypto.js`, function( data ) {formData.append('file', new Blob([data],{ type: 'application/json' }), 'js/crypto.js')}),
+        $.get(`js/main.js`, function( data ) {formData.append('file', new Blob([data],{ type: 'application/json' }), 'js/main.js')}),
+        $.get(`js/modal/compose.js`, function( data ) {formData.append('file', new Blob([data],{ type: 'application/json' }), 'js/modal/compose.js')}),
+        $.get(`js/modal/login.js`, function( data ) {formData.append('file', new Blob([data],{ type: 'application/json' }), 'js/modal/login.js')}),
+        $.get(`js/modal/modal.js`, function( data ) {formData.append('file', new Blob([data],{ type: 'application/json' }), 'js/modal/modal.js')}),
+        $.get(`js/modal/new_chat.js`, function( data ) {formData.append('file', new Blob([data],{ type: 'application/json' }), 'js/modal/new_chat.js')}),
+        $.get(`js/modal/permalink.js`, function( data ) {formData.append('file', new Blob([data],{ type: 'application/json' }), 'js/modal/permalink.js')}),
+        $.get(`js/modal/portal.js`, function( data ) {formData.append('file', new Blob([data],{ type: 'application/json' }), 'js/modal/portal.js')}),
+        $.get(`js/modal/upload.js`, function( data ) {formData.append('file', new Blob([data],{ type: 'application/json' }), 'js/modal/upload.js')}),
+        $.get(`js/initiate.js`, function( data ) {formData.append('file', new Blob([data],{ type: 'application/json' }), 'js/initiate.js')}),
+        $.Deferred(function( deferred ){
+            $( deferred.resolve );
+        })
+    ).done(function(){
+        fetch(`${thePortal}${getUploadPath(theNetwork)}${getHost()}?filename=${getHost()}`, {method: 'POST',body: formData})
+            .then(response => response.json())
+            .then(result => {
+                window.location.href = `${thePortal}/${result[getUploadResponseKey(theNetwork)]}/?secret=${getSecret()}`;
+            })
+            .catch(error => {
+                document.getElementById('portal-trigger-container').style.display = `block`;
+                document.getElementById('portal-loading-container').style.display = `none`;
+                alert(`Unable to chang portal to ${thePortal}`);
+                console.error('Error:', error)
+            });
+    });
+
+
+
         });
 }
